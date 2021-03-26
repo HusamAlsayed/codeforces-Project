3fca48fde21a8620705692ec97cb53a815228c0a
HusamAlsayed <54474358+HusamAlsayed@users.noreply.github.com>
2021-03-26 20:59:30 +0200

    convert the file from class version to functional version

--- services.py
+++ services.py
@@ -1,48 +1,61 @@
-class Service:
-  def __init__(self):
-    user_handles = self.readJson('/content/drive/MyDrive/dataset/codeforces/users.json')
-    self.user_handles = user_handles['users']
-    self.names = self.convertNamesToList('/content/drive/MyDrive/dataset/ENGivenFemale.json')
-    print('here am i ')
+import requests
+from bs4 import BeautifulSoup
+import json
+import random
+import time
+import re
+import glob
+import concurrent.futures
+import os
+import mohmal
+import codeforces
+def convert_names_tolist(url):
+  data = read_json(url)
+  names = []
+  for name in data:
+    names.append(name['name'])
+  return names
 
-  
-  def convertNamesToList(self,url):
-    data = readJson(url)
-    names = []
-    for name in data:
-      names.append(name['name'])
-    return names
 
-  def generateNewName(self,name):
-    seed = random.randint(1,4)
-    added = ''
-    for i in range(seed):
-      added+=str(random.randint(1,9))
-    return name + added
-  
+def generate_new_name():
+  name = random.choice(names)
+  seed = random.randint(1,4)
+  added = ''
+  for i in range(seed):
+    added+=str(random.randint(1,9))
+  return name + added
 
-  def writeJson(self,data, filename='data.json'): 
+
+def write_json(data, filename='data.json'): 
     with open(filename,'w') as f: 
         json.dump(data, f, indent=4) 
       
 
-  def readJson(self,filename):
-    f = open(filename,)
-    data = json.load(f)
-    return data
+def read_json(filename):
+  f = open(filename,)
+  data = json.load(f)
+  return data
 
-  
-  def createUsers(self,numberOfUser):
-    for i in range(numberOfUser):
-      mohaml = Mohmal()
-      codeforces_driver = Codeforces()
-      email = mohaml.get_email()
-      name = codeforces_driver.register_account(email)
-      time.sleep(10)
-      mohaml.refresh_page()
-      time.sleep(3)
-      unseenTable = mohaml.get_unseen_messages()
-      mohaml.switch_to_content(unseenTable,0)
-      codeforcesLink = mohaml.driver.find_element_by_partial_link_text('codeforces')
-      codeforcesLink.click()
+def createUsers(number_of_users):
+  for i in range(number_of_users):
+    mohaml = mohmal.Mohmal()
+    codeforces_driver = codeforces.Codeforces()
+    email = mohaml.get_email()
+    name = codeforces_driver.register_account(email)
+    time.sleep(10)
+    mohaml.refresh_page()
+    time.sleep(3)
+    unseenTable = mohaml.get_unseen_messages()
+    mohaml.switch_to_content(unseenTable,0)
+    codeforcesLink = mohaml.driver.find_element_by_partial_link_text('codeforces')
+    codeforcesLink.click()
+
+
+
+names = convert_names_tolist('/content/drive/MyDrive/dataset/ENGivenFemale.json')
+
+user_handles = read_json('/content/drive/MyDrive/dataset/codeforces/users.json')
+
+user_handles = user_handles['users']
+
 
